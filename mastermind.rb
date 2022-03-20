ec895b986f0fa26bb8c35329ab4adf7d748e8810
Brandon Rasmussen <BrandonRasm0412@gmail.com>
2022-03-20 14:31:12 -0700

    created new main ruby file
    added basic class structure, subject to change

---
+++ mastermind.rb
@@ -0,0 +1,19 @@
+#Base class the runs all the logic and handles turns
+class Game
+
+end
+
+#Basic class that gets input from the player and stores info about them
+class Player
+
+end
+
+#A human player
+class Human < Player
+
+end
+
+#A computer player
+class Comp < Player
+    
+end
