216f1ebc0a465a4d20f2b4b0a7a0c90ce8ad82f9
Brandon Rasmussen <BrandonRasm0412@gmail.com>
2022-03-20 14:46:51 -0700

    added a placeholder get_input method for the Player class and added an actually functional get_input method to the Humanm class

--- mastermind.rb
+++ mastermind.rb
@@ -1,19 +1,15 @@
-#Base class the runs all the logic and handles turns
+# Base class the runs all the logic and handles turns
 class Game
-
 end
 
-#Basic class that gets input from the player and stores info about them
+# Basic class that gets input from the player and stores info about them
 class Player
-
 end
 
-#A human player
+# A human player
 class Human < Player
-
 end
 
-#A computer player
+# A computer player
 class Comp < Player
-    
 end
