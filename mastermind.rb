70e4ea093496f257f089f234ace18c7dbb23b252
Brandon Rasmussen <BrandonRasm0412@gmail.com>
2022-03-20 16:28:35 -0700

    added check guess function that returns an array with colored and white peg count

--- mastermind.rb
+++ mastermind.rb
@@ -1,13 +1,44 @@
 # Base class the runs all the logic and handles turns
 class Game
+  def initialize
+    @player = new Human
+    @code = %w[1 2 3 4]
+  end
+
+  def playRound
+    input = @player.input
+    results = check_guess(input)
+  end
+
+  def check_guess(input)
+    colored_pegs = 0
+    white_pegs = 0
+    input.each_with_index do |number,index|
+        colored_pegs += 1 if @code[index] == number
+
+        if @code[index] == number
+            colored_pegs += 1
+        else @code.include? number
+            white_pegs += 1
+        end
+    end
+    [colored_pegs,white_pegs]
+  end
 end
 
 # Basic class that gets input from the player and stores info about them
 class Player
+  def input
+    [2, 3, 4, 5]
+  end
 end
 
 # A human player
 class Human < Player
+  def input
+    puts 'Input your guess. Type for numbers 1-6'
+    guess = gets.chomp.split
+  end
 end
 
 # A computer player
