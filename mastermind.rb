5d70cade3ade87c6d7080c95479a3e6bbd0d9e21
Brandon Rasmussen <BrandonRasm0412@gmail.com>
2022-03-20 18:49:37 -0700

    added a check_valid function to player input to make sure they didn't crash the program

--- mastermind.rb
+++ mastermind.rb
@@ -5,17 +5,30 @@ class Game
     @code = %w[1 2 3 4]
   end
 
+  def play_game
+    @player.show_instructions
+    round = 1
+    12.times do
+      puts "Round #{round}!"
+      round += 1
+      result = play_round
+      if result
+        puts 'The code was broken!'
+        break
+      end
+    end
+  end
+
   def play_round
     input = @player.input
     results = check_guess(input)
     display_results(results)
+    win = results[0] == 4
   end
 
   def check_guess(input)
     colored_pegs = 0
     white_pegs = 0
-    p input
-    p @code
     code_copy = @code.dup
     input.each_with_index do |number, index|
       if code_copy[index] == number
@@ -39,17 +52,37 @@ end
 # Basic class that gets input from the player and stores info about them
 class Player
   def initialize; end
-
-  def input
-    [2, 3, 4, 5]
-  end
 end
 
 # A human player
 class Human < Player
   def input
-    puts 'Input your guess. Type for numbers 1-6'
-    guess = gets.chomp.split('')
+    valid = false
+    until valid
+      puts 'Input your guess. Type for numbers 1-6'
+      guess = gets.chomp.split('')
+      valid = valid_guess?(guess)
+      puts 'You idiot!' unless valid
+    end
+    guess
+  end
+
+  def valid_guess?(guess)
+    guess.each do |number|
+      number = number.to_i
+      return false unless number.is_a? Integer
+
+      return false if number > 6 || number < 1
+
+      return false unless guess.length == 4
+    end
+    true
+  end
+
+  def show_instructions
+    puts 'Try to guess the code in 12 guesses!'
+    puts 'Colored pegs mean you have the correct number and placement'
+    puts 'White pegs mean correct number but incorrect placement'
   end
 end
 
@@ -58,4 +91,4 @@ class Comp < Player
 end
 
 x = Game.new
-x.play_round
+x.play_game
